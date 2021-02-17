4e800bd9c0c26e031c333bd4bc95e2386b6fa371
JacqPov <jacquelinepovinelli@gmail.com>
2021-02-17 10:46:54 -0300

    ContaBanco

---
+++ ContaBanco/ContaBanco/Conta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBanco
+{
+    class Conta
+    {
+        public int Agencia { get; set; }
+
+        public int NumConta { get; set; }
+
+        public double Saldo { get; set; }
+
+        public void Sacar(double sacar)
+        {
+            if (this.Saldo > sacar)
+            {
+                this.Saldo -= sacar;
+
+            }
+            else
+            {
+                Console.WriteLine("Não é possível sacar!!");
+            }
+
+        }
+
+        public void Deposito(double deposito)
+        {
+            this.Saldo += deposito;
+        }
+
+        //public void Transferir(double transferir)
+        //{
+
+        //}
+    }
+}
